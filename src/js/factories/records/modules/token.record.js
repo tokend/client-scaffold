a1eaf8a7e36a1f489b88fa4d1c6767f66befb550
Vladislav <logvinenko.js@gmail.com>
2018-03-05 19:52:06 +0200

    Remove unused from token.record

--- src/js/factories/records/modules/token.record.js
+++ src/js/factories/records/modules/token.record.js
@@ -1,61 +1,50 @@
-import { RecordFactory } from '../record_factory'
 import config from '../../../../config'
 import get from 'lodash/get'
-import { salesService } from '../../../services/sales.service'
 import { xdr } from 'swarm-js-sdk'
+import { ASSET_POLICIES } from '../../../const/xdr.const'
 
 export class TokenRecord {
-  constructor (record) {
-    this._record = record || { details: { logo: {} } }
-    this.attachedSale = { details: { logo: {} } }
-    this.additionalDetails = {}
+  constructor (record, attachedDetails = {}) {
+    this._record = record
 
-    this.code = this._record.code
-    this.owner = this._record.owner
-    this.available = this._record.available_for_issuance
-    this.name = this._record.details.name
-    this.signer = this._record.preissued_asset_signer
-    this.link = this._record.external_resource_link
-    this.description = this._record.description
-    this.max = this._record.max_issuance_amount
-    this.issued = this._record.issued
+    this.code = record.code
+    this.owner = record.owner
+    this.available = record.available_for_issuance
+    this.name = record.details.name
+    this.signer = record.preissued_asset_signer
+    this.link = record.external_resource_link
+    this.description = record.description
+    this.max = record.max_issuance_amount
+    this.issued = record.issued
     this.policies = this._getPolicies()
-    this.policy = this._record.policy
-  }
+    this.policy = record.policy
 
-  _getPolicies () {
-    return this._record.policies ? this._record.policies.map(policy => policy.value) : []
-  }
-
-  get char () {
-    return this.code[0]
+    this.attachedDetails = attachedDetails
   }
 
   get logoURL () {
-    const url = this._record.details.logo.key || this._record.details.logo.url
-    if (!url) return ''
-    if (url.indexOf(config.FILE_STORAGE) !== -1) return url
-    return `${config.FILE_STORAGE}/${url}`
+    const key = get(this._record, 'details.logo.key')
+    if (!key) return ''
+    return `${config.FILE_STORAGE}/${key}`
   }
 
   get termsDetails () {
     get(this._record, 'details.terms')
   }
 
-  attachSale (sale) {
-    this.attachedSale = sale
-  }
-
-  attachDetails (details) {
-    this.additionalDetails = details
-  }
-
   get requiresKYC () {
     return this._record.policies && this._record.policies.map(policy => policy.value).indexOf(xdr.AssetPolicy.requiresKyc().value) !== -1
   }
 
-  // async:
-  async loadAttachedSale () {
-    this.attachedSale = RecordFactory.createSaleRecord(await salesService.loadSaleByTokenCode(this.code), true)
+  get isWalletToken () {
+    return this.policy & ASSET_POLICIES.baseAsset
+  }
+
+  _getPolicies () {
+    return this._record.policies ? this._record.policies.map(policy => policy.value) : []
+  }
+
+  attachDetails (details) {
+    this.attached = { ...this.attached, ...details }
   }
 }
