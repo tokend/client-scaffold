dbfcf4e1596cfc9954d4a1d1d57ccbae73a53e37
Vladislav <logvinenko.js@gmail.com>
2018-03-05 19:52:23 +0200

    Create wallet overview token list

---
+++ src/vue/app/common/token_list/TokenList.vue
@@ -0,0 +1,29 @@
+<template>
+  <md-list class="md-triple-line">
+    <md-list-item v-for="(token, key) in tokens" :key="key">
+
+      <md-avatar>
+        <img :src="token.logoURL" v-if="token.logoURL">
+        <span v-else>{{ token.code.charAt(0) }}</span>
+      </md-avatar>
+
+      <div class="md-list-item-text">
+        <span>{{ token.code }}</span>
+        <span>{{ token.attachedDetails.balance }}</span>
+        <span>{{ token.attachedDetails.convertedBalance }}</span>
+      </div>
+
+    </md-list-item>
+  </md-list>
+</template>
+
+<script>
+  export default {
+    name: 'token-list',
+    props: { tokens: { type: Array, required: true } }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/vue/app/wallet/overview/Wallet.Overview.vue
+++ src/vue/app/wallet/overview/Wallet.Overview.vue
@@ -1,10 +1,25 @@
 <template>
-  <span>hello</span>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">Wallet tokens</div>
+    </md-card-header>
+    <token-list :tokens="userWalletTokens"/>
+  </md-card>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+  import { vuexTypes } from '../../../../vuex/types'
+  import TokenList from '../../common/token_list/TokenList'
+
   export default {
-    name: 'wallet-overview'
+    name: 'wallet-overview',
+    components: { TokenList },
+    computed: {
+      ...mapGetters([
+        vuexTypes.userWalletTokens
+      ])
+    }
   }
 </script>
 
