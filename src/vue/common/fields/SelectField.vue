9e3583cea971cccf2b48848d2fb3ef059cbb540a
Vladislav <logvinenko.js@gmail.com>
2018-03-05 17:53:39 +0200

    Add mixin props to select

--- src/vue/common/fields/SelectField.vue
+++ src/vue/common/fields/SelectField.vue
@@ -1,11 +1,19 @@
 <template>
   <md-field>
     <md-select
+      :placeholder="placeholder"
+      :disabled="disabled"
       :multiple="multiple"
+      :required="required"
       :md-dense="dense"
+      :value="value"
+      :name="name"
+      :id="id"
+      @change="onInput"
     >
       <md-option v-for="(value, i) in values" :value="value" :key="i">{{ value }}</md-option>
     </md-select>
+    <span class="md-error" v-if="errorMessage">{{ errorMessage }}</span>
   </md-field>
 </template>
 
