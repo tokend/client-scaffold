e1acfb90fa506ab4ba4a0d7573f7d6ffb120f160
Vladislav <logvinenko.js@gmail.com>
2018-03-05 17:48:26 +0200

    Add select field

---
+++ src/vue/common/fields/SelectField.vue
@@ -0,0 +1,28 @@
+<template>
+  <md-field>
+    <md-select
+      :multiple="multiple"
+      :md-dense="dense"
+    >
+      <md-option v-for="(value, i) in values" :value="value" :key="i">{{ value }}</md-option>
+    </md-select>
+  </md-field>
+</template>
+
+<script>
+  import FieldMixin from './field.mixin'
+
+  export default {
+    name: 'select-field',
+    mixins: [FieldMixin],
+    props: {
+      values: { type: Array, default: _ => [] },
+      multiple: { type: Boolean, default: false },
+      dense: { type: Boolean, default: false }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
