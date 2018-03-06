6afe6330fc50153a4a7ac381d63f8bd43d61a984
Vladislav <logvinenko.js@gmail.com>
2018-03-06 21:19:50 +0200

    Upd const with default asset

--- src/js/const/configs.const.js
+++ src/js/const/configs.const.js
@@ -8,3 +8,4 @@ export const DEFAULT_INPUT_MIN = `0.${'0'.repeat(DEFAULT_PRECISION - 1)}1`
 export const DEFAULT_DATE_FORMAT = 'DD MMM YYYY'
 export const DEFAULT_DATE_TIME_FORMAT = 'DD MMM YYYY [at] HH:mm:ss'
 export const DEFAULT_MAX_AMOUNT = Operation.MAX_INT64_AMOUNT
+export const DEFAULT_SELECTED_ASSET = 'BTC'
