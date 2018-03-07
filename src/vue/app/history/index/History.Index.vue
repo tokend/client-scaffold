3d657d00311aba8fcc6634dc4262f8fce771589c
Vladislav <logvinenko.js@gmail.com>
2018-03-07 16:03:00 +0200

    Render tx table

--- src/vue/app/history/index/History.Index.vue
+++ src/vue/app/history/index/History.Index.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="tx-history">
-    tx history
+    <md-table v-model="list" md-card class="tx-history__table">
+      <md-table-toolbar>
+        <h1 class="md-title">Transaction history</h1>
+      </md-table-toolbar>
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell class="tx-history__table-cell" md-label="Date">{{ item.date }}</md-table-cell>
+        <md-table-cell class="tx-history__table-cell" md-label="Transaction">{{ item.name }}</md-table-cell>
+        <md-table-cell class="tx-history__table-cell" md-label="Status">{{ item.state }}</md-table-cell>
+        <md-table-cell class="tx-history__table-cell" md-label="Asset">{{ item.asset }}</md-table-cell>
+        <md-table-cell class="tx-history__table-cell" md-label="Amount">{{ item.amount }}</md-table-cell>
+        <md-table-cell class="tx-history__table-cell tx-history__table-cell--counterparty" md-label="Counterparty">
+          {{ item.counterparty }}
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
 <script>
   import { mapGetters, mapActions } from 'vuex'
   import { vuexTypes } from '../../../../vuex/types'
+  import { RecordTypes } from '../../../../js/records/types'
+  import get from 'lodash/get'
 
   export default {
     name: 'history-index',
@@ -21,7 +38,15 @@
         vuexTypes.transactions
       ]),
       list () {
-        return this.transactions[this.tokenCode]
+        return (get(this.transactions, `${this.tokenCode}.records`) || [])
+          .reduce((list, item) => {
+            if (item instanceof RecordTypes.MatchRecord) {
+              item.transactions.forEach(tx => { list.push(tx) })
+              return list
+            }
+            list.push(item)
+            return list
+          }, [])
       }
     },
     methods: {
@@ -37,6 +62,19 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import '../../../../scss/variables';
 
+  .tx-history__table {
+    .md-table-content {
+      background: $col-md-block !important;
+    }
+  }
+
+  .tx-history__table-cell {
+    overflow: hidden;
+    &--counterparty {
+      max-width: 10rem;
+    }
+  }
 </style>
