dbc834e61babebd5b9a3c3240d3609e24070f0bf
Vladislav <logvinenko.js@gmail.com>
2018-03-07 14:21:55 +0200

    Map tx-module with History component

--- src/vue/app/history/index/History.Index.vue
+++ src/vue/app/history/index/History.Index.vue
@@ -5,8 +5,35 @@
 </template>
 
 <script>
+  import { mapGetters, mapActions } from 'vuex'
+  import { vuexTypes } from '../../../../vuex/types'
+
   export default {
-    name: 'history-index'
+    name: 'history-index',
+    data: _ => ({
+      tokenCode: 'BTC'
+    }),
+    created () {
+      this.loadList(this.tokenCode)
+    },
+    computed: {
+      ...mapGetters([
+        vuexTypes.transactions
+      ]),
+      list () {
+        return this.transactions[this.tokenCode]
+      }
+    },
+    methods: {
+      ...mapActions({
+        loadList: vuexTypes.GET_TX_LIST
+      })
+    },
+    watch: {
+      tokenCode (code) {
+        this.loadList(code)
+      }
+    }
   }
 </script>
 
