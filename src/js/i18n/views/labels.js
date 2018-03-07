a84b926145c85428479555d4fb156f7c4695da53
Vladislav <logvinenko.js@gmail.com>
2018-03-07 16:30:23 +0200

    Use v-for instead of slots and use i18n labels in headers

--- src/js/i18n/views/labels.js
+++ src/js/i18n/views/labels.js
@@ -3,5 +3,9 @@ export default {
   lbl_pwd: 'Password',
   lbl_confirm: 'Confirm password',
   lbl_asset: 'Asset',
-  lbl_amount: 'Amount'
+  lbl_amount: 'Amount',
+  lbl_date: 'Date',
+  lbl_tx_type: 'Transaction type',
+  lbl_status: 'Status',
+  lbl_counterparty: 'Counterparty'
 }
--- src/vue/app/history/index/History.Index.vue
+++ src/vue/app/history/index/History.Index.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="tx-history">
-    <md-table v-model="list" md-card class="tx-history__table">
+    <md-table md-card class="tx-history__table">
       <md-table-toolbar>
         <h1 class="md-title">Transaction history</h1>
       </md-table-toolbar>
 
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell class="tx-history__table-cell" md-label="Date">{{ item.date }}</md-table-cell>
-        <md-table-cell class="tx-history__table-cell" md-label="Transaction">{{ item.name }}</md-table-cell>
-        <md-table-cell class="tx-history__table-cell" md-label="Status">{{ item.state }}</md-table-cell>
-        <md-table-cell class="tx-history__table-cell" md-label="Asset">{{ item.asset }}</md-table-cell>
-        <md-table-cell class="tx-history__table-cell" md-label="Amount">{{ item.amount }}</md-table-cell>
-        <md-table-cell class="tx-history__table-cell tx-history__table-cell--counterparty" md-label="Counterparty">
-          {{ item.counterparty }}
-        </md-table-cell>
-        <md-table-cell>
-          <md-button class="md-icon-button" @click="openDetails">
-            <md-icon>keyboard_arrow_down</md-icon>
-          </md-button>
-        </md-table-cell>
+      <md-table-row>
+        <md-table-head>{{ i18n.lbl_date() }}</md-table-head>
+        <md-table-head>{{ i18n.lbl_tx_type() }}</md-table-head>
+        <md-table-head>{{ i18n.lbl_status() }}</md-table-head>
+        <md-table-head>{{ i18n.lbl_asset() }}</md-table-head>
+        <md-table-head>{{ i18n.lbl_amount() }}</md-table-head>
+        <md-table-head><!--Button--></md-table-head>
       </md-table-row>
+
+      <template v-for="item in list">
+
+        <md-table-row>
+          <md-table-cell class="tx-history__table-cell">{{ item.date }}</md-table-cell>
+          <md-table-cell class="tx-history__table-cell">{{ item.name }}</md-table-cell>
+          <md-table-cell class="tx-history__table-cell">{{ item.state }}</md-table-cell>
+          <md-table-cell class="tx-history__table-cell">{{ item.asset }}</md-table-cell>
+          <md-table-cell class="tx-history__table-cell">{{ item.amount }}</md-table-cell>
+          <md-table-cell class="tx-history__table-cell tx-history__table-cell--counterparty" md-label="Counterparty">
+            {{ item.counterparty }}
+          </md-table-cell>
+
+          <md-table-cell>
+            <md-button class="md-icon-button" @click="openDetails">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-table-cell>
+
+        </md-table-row>
+
+      </template>
     </md-table>
   </div>
 </template>
@@ -28,12 +43,15 @@
   import { mapGetters, mapActions } from 'vuex'
   import { vuexTypes } from '../../../../vuex/types'
   import { RecordTypes } from '../../../../js/records/types'
+  import { i18n } from '../../../../js/i18n'
   import get from 'lodash/get'
 
   export default {
     name: 'history-index',
     data: _ => ({
-      tokenCode: 'BTC'
+      tokenCode: 'BTC',
+      selectedItem: '',
+      i18n
     }),
     created () {
       this.loadList(this.tokenCode)
@@ -57,7 +75,10 @@
     methods: {
       ...mapActions({
         loadList: vuexTypes.GET_TX_LIST
-      })
+      }),
+      openDetails (item) {
+        this.selectedItem = this.selectedItem ? null : item.id
+      }
     },
     watch: {
       tokenCode (code) {
