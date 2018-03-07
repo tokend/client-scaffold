3f3c209dac9491ff3a767117f9e7b97d9985d4fd
Dmitry Sviridenko <letehaha@gmail.com>
2018-03-07 19:30:06 +0200

    Add ability to sign out

--- src/js/i18n/views/labels.js
+++ src/js/i18n/views/labels.js
@@ -3,5 +3,10 @@ export default {
   lbl_pwd: 'Password',
   lbl_confirm: 'Confirm password',
   lbl_asset: 'Asset',
-  lbl_amount: 'Amount'
+  lbl_amount: 'Amount',
+  lbl_settings: 'Settings',
+  lbl_signout: 'Sign Out',
+  lbl_account: 'My Account',
+  lbl_verified: 'Verified user',
+  lbl_unverified: 'Unverified user'
 }
--- src/vue/navigation/Navbar.vue
+++ src/vue/navigation/Navbar.vue
@@ -18,20 +18,47 @@
     </div>
   </nav>
 
-  <nav class="md-layout md-alignment-bottom-left" v-else>
-    <md-tabs class="md-layout-item md-primary" md-sync-route>
+  <nav class="md-layout navbar" v-else>
+    <md-tabs class="md-layout md-primary md-alignment-top-right" md-sync-route>
       <md-tab v-for="(route, key) in routes"
               :md-label="route.label"
               :to="route.path"
               :key="key"
       />
     </md-tabs>
+    <div class="md-layout md-layout-item md-alignment-center-right">
+      <div class="navbar__user">
+        <md-button @click='toggleUserCardVisibility' class="md-icon-button md-dense md-primary">
+          <md-icon>account_circle</md-icon>
+        </md-button>
+        <md-card class="navbar__user-card" v-show="isUserCardOpen">
+          <md-card-content>
+            <div class="navbar__user-card-content">
+              <div class="navbar__user-avatar">
+                {{ userEmail.substr(0, 1).toUpperCase() }}
+              </div>
+              <div class="navbar__user-info">
+                <p class="navbar__user-name">{{ userEmail }}</p>
+                <p class="navbar__user-status" v-if="userType === 'not_verified'">{{ i18n.lbl_unverified() }}</p>
+                <p class="navbar__user-status" v-else>{{ i18n.lbl_verified() }}</p>
+                <md-button class="md-raised md-primary navbar__user-account-btn">{{ i18n.lbl_account() }}</md-button>
+              </div>
+            </div>
+            <div class="navbar__user-actions md-layout md-alignment-center-space-between">
+              <md-button class="navbar__user-action-btn md-raised">{{ i18n.lbl_settings() }}</md-button>
+              <md-button class="navbar__user-action-btn md-raised" @click="signOut">{{ i18n.lbl_signout() }}</md-button>
+            </div>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
   import { vuexTypes } from '../../vuex/types'
-  import { mapGetters } from 'vuex'
+  import { i18n } from '../../js/i18n'
+  import { mapActions, mapGetters } from 'vuex'
   import { commonEvents } from '../../js/events/common_events'
   import { attachEventHandler } from '../../js/events/helpers'
 
@@ -39,12 +66,16 @@
     name: 'root-navbar',
 
     data: () => ({
-      routes: []
+      routes: [],
+      isUserCardOpen: false,
+      i18n
     }),
 
     computed: {
       ...mapGetters([
-        vuexTypes.isLoggedIn
+        vuexTypes.isLoggedIn,
+        vuexTypes.userEmail,
+        vuexTypes.userType
       ])
     },
 
@@ -53,15 +84,160 @@
     },
 
     methods: {
+      ...mapActions([
+        vuexTypes.LOG_OUT
+      ]),
       updateRoutes (routes) {
         this.routes = routes
+      },
+      toggleUserCardVisibility () {
+        this.isUserCardOpen = !this.isUserCardOpen
+      },
+      signOut () {
+        this.LOG_OUT()
       }
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../../scss/mixins";
+
   nav {
     min-height: 64px;
+  }
+
+  .navbar {
+    width: 100%;
+  }
+
+  .navbar__user {
+    position: relative;
+  }
+
+  .navbar__user-card {
+    position: absolute;
+    right: 10px;
+    top: calc(100% + 14px);
+    overflow: visible;
+    padding: 24px 24px 18px 24px;
+
+    & > .md-card-content {
+      padding: 0;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 0 8px 8px 8px;
+      border-color: transparent transparent #ffffff transparent;
+      top: -8px;
+      right: 4px;
+    }
+
+    @include respond-to-custom(800px) {
+      width: calc(100vw - 230px - 24px); // 230px - sidebar width
+    }
+
+    @include respond-to(small) {
+      padding: 16px;
+      width: calc(100vw - 24px);
+      max-width: 404px;
+    }
+  }
+
+  .navbar__user-card-content {
+    display: flex;
+    padding-bottom: 21px;
+    position: relative;
+
+    @include respond-to(small) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+
+  .navbar__user-actions {
+    position: relative;
+    padding-top: 16px;
+
+    &:after {
+      content: '';
+      position: absolute;
+      height: 2px;
+      width: calc(100% + 48px);
+      background-color: #e8e8e8;
+      left: -24px;
+      top: 0;
+    }
+  }
+
+  .navbar__user-avatar {
+    width: 102px;
+    height: 102px;
+    border-radius: 50%;
+    font-size: 48px;
+    color: #fff;
+    background-color: #ccc;
+    margin-right: 27px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: none;
+
+    @include respond-to(small) {
+      margin-right: 0;
+      margin-bottom: 12px;
+    }
+  }
+
+  .navbar__user-name {
+    font-size: 16px;
+    line-height: 1;
+    text-align: left;
+    color: #000;
+    margin-bottom: 12px;
+
+    @include respond-to-custom(800px) {
+      word-break: break-word;
+    }
+
+    @include respond-to(small) {
+      word-break: break-word;
+    }
+  }
+
+  .navbar__user-status {
+    font-size: 12px;
+    line-height: 1;
+    text-align: left;
+    color: rgba(0, 0, 0, .5);
+    margin-bottom: 26px;
+
+    @include respond-to(small) {
+      text-align: center;
+    }
+  }
+
+  .navbar__user-account-btn {
+    margin: 0;
+
+    @include respond-to(small) {
+      display: block;
+      margin-right: auto;
+      margin-left: auto;
+
+      &:last-child {
+        margin-right: auto;
+        margin-left: auto;
+      }
+    }
+  }
+
+  .navbar__user-action-btn {
+    color: rgba(0, 0, 0, .5);
   }
 </style>
