3517ba049012279ff73d42603588ed63786077cf
Vladislav <logvinenko.js@gmail.com>
2018-03-20 15:11:49 +0200

    Fix clipboard target mismatch

--- src/vue/app/common/Detail.Row.vue
+++ src/vue/app/common/Detail.Row.vue
@@ -2,7 +2,7 @@
   <p class="detail-row">
     <span class="detail-row__key">{{ prop }}</span>
     <span class="detail-row__value">
-      <span class="detail-row__value-text" id="clipboard-target">{{ value }}</span>
+      <span class="detail-row__value-text" :id="copiable ? 'clipboard-target' : ''">{{ value }}</span>
       <md-button
         v-if="copiable"
         class="detail-row__clipboard-btn md-icon-button"
