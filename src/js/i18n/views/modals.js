64a2858b95a4c25bb829f7af575f57a068b43c35
Vladislav <logvinenko.js@gmail.com>
2018-03-21 19:06:24 +0200

    Add async confirm action

--- src/js/i18n/views/modals.js
+++ src/js/i18n/views/modals.js
@@ -5,5 +5,9 @@ export default {
   mod_save_seed: 'Save this seed to your offline device',
   mod_why_save_seed: `For security reasons, please put this seed in some secret place.
                       You will need it to recover access in case if your account will be lost.`,
-  mod_validate_seed_how: 'Please validate this seed in the field below to ensure that you copied it properly'
+  mod_validate_seed_how: 'Please validate this seed in the field below to ensure that you copied it properly',
+  mod_confirm_title: 'Are you sure?',
+  mod_confirm_message: 'Please confirm this action before continuing',
+  mod_confirm_confirm_text: 'Confirm',
+  mod_confirm_cancel_text: 'Cancel'
 }
