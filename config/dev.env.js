2d9274c29d7e25fb7df1757614d06819b08e9019
Vladislav <logvinenko.js@gmail.com>
2018-03-29 20:35:55 +0300

    Https

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"dev"',
-  HORIZON_SERVER: '"api.mobitile.tokend.org"',
-  FILE_STORAGE: '"storage.mobitile.tokend.org"',
+  HORIZON_SERVER: '"https://api.mobitile.tokend.org"',
+  FILE_STORAGE: '"https://storage.mobitile.tokend.org"',
   NETWORK_PASSPHRASE: '"TokenD Demo Network"'
 }
