0021ca79a5d0c6120c7d2d2d5c80220e974e4675
Vladislav <logvinenko.js@gmail.com>
2018-04-09 17:54:47 +0300

    Restore kyc-selfie

--- src/vue/app/verification/spec/kyc-individual.schema.js
+++ src/vue/app/verification/spec/kyc-individual.schema.js
@@ -131,6 +131,13 @@ export const kycIndividualSchema = {
       "side": "front",
       "private": true,
       "label": i18n.lbl_kyc_proof()
+    },
+    {
+      "id": "kyc-file-photo",
+      "type": documentTypes.kycSelfie,
+      "side": "front",
+      "private": true,
+      "label": i18n.lbl_kyc_selfie()
     }
   ]
 }
