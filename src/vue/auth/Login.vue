0ff1902e652035bc004f9cdf2071be6126dc2000
Vladislav <logvinenko.js@gmail.com>
2018-04-25 17:33:48 +0300

    Lowercase emails on login

--- src/vue/auth/Login.vue
+++ src/vue/auth/Login.vue
@@ -121,7 +121,7 @@
 
       async submit () {
         if (!await this.isValid()) return
-        this.form.emali = this.form.email.toLowerCase()
+        this.form.email = this.form.email.toLowerCase()
         this.disable()
         try {
           await this.processUserWallet(this.form)
