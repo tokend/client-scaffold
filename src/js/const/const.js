a2ec418e04c56567907af05b22ca434be9ebfa21
Unknown <datik94@gmail.com>
2018-07-05 20:11:59 +0300

    Add document_policies, document wrappers

--- src/js/const/const.js
+++ src/js/const/const.js
@@ -23,6 +23,8 @@ export { ISSUANCE_REQUEST_STATES } from './issuance_request_states.const'
 export { REQUEST_STATES, REQUEST_STATES_STR } from './request_states.const'
 export { SIGNER_TYPES } from './signer_types.const'
 export { ACCOUNT_STATES } from './account.const'
+export { DOCUMENT_POLICIES } from './document-policies.const'
+
 export {
   REQUEST_TYPES,
   FEE_TYPES,
