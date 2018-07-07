fc90dfb68fb9459d83fc163584a07390f1e955d6
Unknown <datik94@gmail.com>
2018-07-07 16:28:18 +0300

    Remove unused lines

--- src/js/records/kyc_corporate_request.record.js
+++ src/js/records/kyc_corporate_request.record.js
@@ -1,8 +1,4 @@
-// import { RequestRecord } from './request.record'
-// import config from '../../config'
-// import _get from 'lodash/get'
 import { wrapDocuments, unwrapDocuments } from '../../js/helpers/DocumentContainer'
-// import { Keypair } from 'swarm-js-sdk'
 export class KycCorporateRequestRecord {
   constructor (record = {}) {
     this._record = record
@@ -12,7 +8,7 @@ export class KycCorporateRequestRecord {
     this.phoneNumber = record.phone_number || ''
     this.country = record.country || ''
     this.city = record.city || ''
-    this.address = record.address || ''
+    this.street = record.street || ''
     this.postalCode = record.postal_code || ''
     this.firstName = record.first_name || ''
     this.lastName = record.last_name || ''
@@ -23,15 +19,6 @@ export class KycCorporateRequestRecord {
     this.residenceAddress = record.residence_address || ''
     this.residencePostalCode = record.residence_postal_code || ''
     this.documents = wrapDocuments(record.documents)
-    // this.kycOriginationCertificate = this._record.kyc_origination_certificate || ''
-    // this.kycShareholdersCertificate = this._record.kyc_shareholders_certificate || ''
-    // this.kycAnnualReport = this._record.kyc_annual_report || ''
-    // this.kycIdDocument = this._record.kyc_id_document || ''
-    // this.kycProofOfAddress = this._record.kyc_proof_of_address || ''
-    // this.kycMemorandium = this._record.kyc_memorandium || ''
-    // this.kycSignatoriesAuthorization = this._record.kyc_signatories_authorization || ''
-    // this.kycOrganizationChart = this._record.kyc_organization_chart || ''
-    // this.kycBusinessModel = this._record.kyc_business_model || ''
   }
   getDetailsForSave () {
     return {
@@ -41,7 +28,7 @@ export class KycCorporateRequestRecord {
       phone_number: this.phoneNumber,
       country: this.country,
       city: this.city,
-      address: this.address,
+      street: this.street,
       postal_code: this.postalCode,
       first_name: this.firstName,
       last_name: this.lastName,
@@ -52,15 +39,6 @@ export class KycCorporateRequestRecord {
       residence_address: this.residenceAddress,
       residence_postal_code: this.residencePostalCode,
       documents: unwrapDocuments(this.documents)
-      // kyc_origination_certificate: this.kycOriginationCertificate,
-      // kyc_shareholders_certificate: this.kycShareholdersCertificate,
-      // kyc_annual_report: this.kycAnnualReport,
-      // kyc_id_document: this.kycIdDocument,
-      // kyc_proof_of_address: this.kycProofOfAddress,
-      // kyc_memorandium: this.kycMemorandium,
-      // kyc_signatories_authorization: this.kycSignatoriesAuthorization,
-      // kyc_organization_chart: this.kycOrganizationChart,
-      // kyc_business_model: this.kycBusinessModel
     }
   }
 }
