73089882e9e6f9666e43189ca331a143fa0615a3
Vladislav <logvinenko.js@gmail.com>
2018-07-12 17:52:08 +0300

    Separate mass-transfers from common transfers

--- src/vue/app/massTransfers/MassTransfers.Make.vue
+++ src/vue/app/massTransfers/MassTransfers.Make.vue
@@ -1,12 +1,75 @@
 <template>
-  <div>
-    mass-transfer
+  <div class="mass-transfer">
+    <file-field class="mass-transfer__upload-input"
+                v-model="documents.payment"
+                label="Select File(s)"
+                accept=".csv"
+                id="preissuance-field"
+    />
+    <ul class="mass-transfer__list" v-if="payments.length">
+      <p>{{ i18n.lbl_to_upload() }}</p>
+
+      <li v-for="(item, index) in payments" :key="index">
+        {{index + 1}}. {{item.account}} {{i18n.c(item.amount)}} {{item.asset}} {{item.email}} {{item.id}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import { FileHelper } from '../../../js/helpers/file.helper'
+  import { Keypair } from 'swarm-js-sdk'
+  import { accountsService } from '../../../js/services/accounts.service'
+
+  import { i18n } from '../../../js/i18n'
+
+  import FileField from '../../common/fields/FileField'
+  import FormMixin from '../../common/mixins/form.mixin'
+
   export default {
-    name: 'MassTransfers.Index'
+    name: 'MassTransfersIndex',
+    components: { FileField },
+    mixins: [FormMixin],
+    data: _ => ({
+      documents: {
+        payment: null
+      },
+      payments: [],
+      i18n
+    }),
+    methods: {
+      parsePayments (payments) {
+        payments.forEach(async (value) => {
+          if (Keypair.isValidPublicKey(value.account)) {
+            value.email = await accountsService.loadEmailByAccountId(value.account)
+            value.id = value.account
+          } else {
+            value.id = await accountsService.loadAccountIdByEmail(value.account)
+            value.email = value.account
+          }
+        })
+        console.log(payments)
+        this.payments = payments
+      }
+    },
+    watch: {
+      'documents.payment': async function (value) {
+        if (value) {
+          const extracted = await FileHelper.readFileAsText(value.file)
+          const extractedArr = extracted.replace(/\r\n/g, '\n').split('\n')
+          const parsedPayment = extractedArr.map((payment) => {
+            const split = payment.split(',')
+            const objKeys = ['account', 'amount', 'asset']
+            const converted = split.reduce((obj, item, i) => {
+              obj[objKeys[i]] = item
+              return obj
+            }, {})
+            return converted
+          })
+          this.parsePayments(parsedPayment)
+        }
+      }
+    }
   }
 </script>
 
