3a6781548abf43f557ceeb1d2507aabcc89bfeeb
Unknown <datik94@gmail.com>
2018-07-17 17:48:47 +0300

    Add labels

--- src/js/i18n/views/sale_view.js
+++ src/js/i18n/views/sale_view.js
@@ -60,5 +60,9 @@ export default {
   sale_upload_success: 'Document uploaded successfully',
   sale_doc_tab_title_is_my: 'Valid legal documents allow investors to build trust in your corporate',
   sale_doc_tab_docs_is_my: 'Uploaded documents will be listed here',
-  sale_doc_tab_docs_desc: 'Valid legal documents allow investors to build trust in the asset manager'
+  sale_doc_tab_docs_desc: 'Valid legal documents allow investors to build trust in the asset manager',
+  sale_upd_tab_add_update: 'Add update to the timeline',
+  sale_upd_tab_add_timeline: 'Add item to timeline',
+  sale_upd_tab_share_updates: 'Share your updates with your backers and potential investors!',
+  sale_upd_tab_important_updated: 'Important updates will be posted here'
 }
