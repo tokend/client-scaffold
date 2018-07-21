dfdfaba8083f0fc86a02f5003fe5865fc0d052a9
Maksym Nesterov <maxym.nes.dev@gmail.com>
2018-07-21 18:59:48 +0300

    Recolor TextareaField

--- src/vue/common/fields/TextareaField.vue
+++ src/vue/common/fields/TextareaField.vue
@@ -1,6 +1,6 @@
 <template>
-  <md-field>
-    <label :for="id" v-if="label">{{ label }}</label>
+  <md-field class="textarea-field">
+    <label class="textarea-field__label" :for="id" v-if="label">{{ label }}</label>
     <md-textarea
       :placeholder="placeholder"
       :disabled="disabled"
@@ -11,6 +11,7 @@
       :name="name"
       :id="id"
       @input="onInput"
+      class="textarea-field__textarea"
     />
     <span class="md-error" v-if="errorMessage">{{ errorMessage }}</span>
   </md-field>
@@ -29,6 +30,28 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import '~@scss/variables.scss';
+  @import './scss/fields-variables.scss';
 
+  .textarea-field:after {
+    border-color: $col-md-primary !important;
+  }
+
+  .textarea-field__label {
+    @include text-font-sizes;
+    color: $field-color-unfocused !important;
+  }
+
+  .textarea-field.md-focused > .textarea-field__label,
+  .textarea-field.md-has-value > .textarea-field__label {
+    @include label-font-sizes;
+    color: $field-color-focused !important;
+  }
+
+  .textarea-field__textarea {
+    @include text-font-sizes;
+    color: $col-md-primary !important;
+    -webkit-text-fill-color: $col-md-primary !important;
+  }
 </style>
