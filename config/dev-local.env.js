a9c64a097038e7c4f58fe28728f1aa20c6270ecb
Maksym Nesterov <maxym.nes.dev@gmail.com>
2018-07-25 20:49:38 +0300

    Fix local env

--- config/dev-local.env.js
+++ config/dev-local.env.js
@@ -2,8 +2,5 @@ const merge = require('webpack-merge')
 const env = require('./default.env')
 
 module.exports = merge(env, {
-  NODE_ENV: '"development"',
-  HORIZON_SERVER: '"https://api-dev.swarm.fund"',
-  FILE_STORAGE: '"https://storage-dev.swarm.fund/api"',
-  NETWORK_PASSPHRASE: '"Swarm New Dev; Feb 18"'
+  NODE_ENV: '"dev"'
 })
