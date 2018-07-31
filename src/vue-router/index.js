9afa1f6f93fc20c967c7bbf0aff14cf60d76168d
Unknown <datik94@gmail.com>
2018-07-31 20:25:04 +0300

    Show mass transfer component name

--- src/vue-router/index.js
+++ src/vue-router/index.js
@@ -183,6 +183,7 @@ const router = new Router({
             {
               path: '/mass-transfers/make',
               name: 'mass-transfers.make',
+              meta: { pageName: PAGES_NAMES.massTransfer },
               component: MassTransfersMake
             }
           ]
