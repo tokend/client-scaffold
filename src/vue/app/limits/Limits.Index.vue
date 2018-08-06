4dc1676c0af162733698d82cbe0230e1ac7b58b1
Unknown <datik94@gmail.com>
2018-08-06 16:02:26 +0300

    Expand component width
    (cherry picked from commit 1395a5b4181b2022d14eeeee0d6df2edebfd5172)

--- src/vue/app/limits/Limits.Index.vue
+++ src/vue/app/limits/Limits.Index.vue
@@ -91,7 +91,7 @@
     flex: 1;
     width: 100%;
     background-color: $col-content-block;
-    max-width: calc(65 * 0.625rem);
+    max-width: calc(80 * 0.625rem);
     margin: auto;
     border-radius: 2px;
     box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
