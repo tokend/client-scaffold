a32a79121af6d42f5cadbd176b2d9414c7e6feb2
Valeria Kiseleva <lja.18n@gmail.com>
2018-08-22 11:54:03 +0300

    added class "button-flat" to continue-button

--- src/vue/auth/ConfirmEmail.vue
+++ src/vue/auth/ConfirmEmail.vue
@@ -16,7 +16,7 @@
               v-ripple>
         {{ i18n.lbl_request_new_email() }}
       </button>
-      <router-link class="auth-page__submit-btn"
+      <router-link class="auth-page__submit-btn button-flat"
                    :to="vueRoutes.login"
                    tag="button">
         {{ i18n.lbl_continue() }}
