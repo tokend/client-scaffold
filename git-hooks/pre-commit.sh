553b12e0b03aea16b8649db3ef3d70c64051222b
Dmitry Sviridenko <letehaha@gmail.com>
2018-09-01 13:39:45 +0300

    Added git hooks and setup npm commads to setup git hooks

---
+++ git-hooks/pre-commit.sh
@@ -0,0 +1,28 @@
+# !/bin/sh
+
+files=$(git diff --cached --name-only --diff-filter=ACM | grep -E "(.js|.vue)$")
+if [ "$files" == "" ]; then
+  exit 0
+fi
+
+lintfiles=""
+errors="false"
+
+for file in ${files}; do
+  if [[ $file != *".eslintrc.js"* ]]; then
+    ./node_modules/.bin/eslint --max-warnings 0 $file
+
+    if [[ $? -ne 0 ]]; then
+      lintfiles+="\n$file"
+      errors="true"
+    fi
+  fi
+
+done
+
+if [ "$errors" == "true" ]; then
+  echo "\n\033[41m COMMIT FAILED: \033[0m Your commit contains lint errors and/or warnings. Fix it before push"
+  exit 1
+fi
+
+exit 0
