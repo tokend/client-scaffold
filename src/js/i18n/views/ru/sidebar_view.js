d73e35b1aba868e5d7f8aff90e819b20839be3e8
Vladislav <logvinenko.js@gmail.com>
2018-10-23 19:52:37 +0300

    Add some translations

---
+++ src/js/i18n/views/ru/sidebar_view.js
@@ -0,0 +1,9 @@
+export default {
+  sidebar_section_account: 'My account',
+  sidebar_section_explore: 'Explorations',
+  sidebar_section_corporate: 'Corporate',
+  sidebar_manage_docs: 'Управление документами',
+  sidebar_upload_docs: 'Загрузить',
+  sidebar_explore_docs: 'Список',
+  sidebar_check_docs: 'Найти'
+}
