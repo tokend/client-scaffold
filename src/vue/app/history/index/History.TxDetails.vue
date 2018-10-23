a562186b39f7ebae5b8016f501b8f1f5171b8c6e
Unknown <datik94@gmail.com>
2018-10-23 15:44:14 +0300

    Remove legacy details showing components
    (cherry picked from commit a2f62761ef0b9f678af14a1def84157f2addebf7)

--- src/vue/app/history/index/History.TxDetails.vue
+++ src/vue/app/history/index/History.TxDetails.vue
@@ -1,35 +1,20 @@
 <template>
-  <transfer
-    v-if="tx instanceof Transfer"
-    :tx="tx" />
-  <match
-    v-else-if="tx instanceof Match"
-    :tx="tx" />
   <record-details-viewer
-    v-else
     :tx="tx" />
 </template>
 
 <script>
-import Transfer from './components/Details.Transfer'
-import Match from './components/Details.Match'
 import RecordDetailsViewer from '../../common/RecordDetailsViewer'
-
-import { RecordTypes } from '../../../../js/records/types'
 
 export default {
   name: 'tx-details',
   components: {
-    Transfer,
-    Match,
     RecordDetailsViewer
   },
   props: {
     tx: { type: Object, required: true }
   },
   data: _ => ({
-    Transfer: RecordTypes.TransferRecord,
-    Match: RecordTypes.MatchTransaction
   })
 }
 </script>
