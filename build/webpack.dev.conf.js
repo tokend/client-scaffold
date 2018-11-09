caaada12347ce7dde0f448bbfaa5e0393b0b541e
Vladislav <logvinenko.js@gmail.com>
2018-11-09 14:22:00 +0200

    Move noEmitOnErrors plugin to optimization

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,7 +28,6 @@ module.exports = merge(baseWebpackConfig, {
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
