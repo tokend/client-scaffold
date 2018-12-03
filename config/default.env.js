fd3329761ff93e16b7f8a6691fe76b5a7e158475
Vladislav <logvinenko.js@gmail.com>
2018-12-03 14:44:33 +0200

    Upd configs

--- config/default.env.js
+++ config/default.env.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: '"default"',
-  HORIZON_SERVER: '"https://testnet.tokend.org/_/api"',
-  FILE_STORAGE: '"https://storage.testnet.tokend.org/api"',
-  NETWORK_PASSPHRASE: '"TokenD Testnet Network"'
+  HORIZON_SERVER: '"http://localhost:8000"',
+  FILE_STORAGE: '"http://localhost:9000"',
+  PSIM_SERVER: '"http://localhost:8089"',
+  NETWORK_PASSPHRASE: '"TokenD Developer Network"'
 }
--- src/config.js
+++ src/config.js
@@ -22,6 +22,7 @@ const featureFlags = {
 
 export default Object.assign(
   {
+    PSIM_SERVER: '',
     HORIZON_SERVER: '',
     FILE_STORAGE: '',
     NETWORK_PASSPHRASE: '',
