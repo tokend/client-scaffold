056588ad0791db931e6efaba50ba8f77c86a8dd4
Vladislav <logvinenko.js@gmail.com>
2018-12-03 19:33:50 +0200

    Add pagination to deposit list

--- src/vue/app/deposit/Deposit.List.vue
+++ src/vue/app/deposit/Deposit.List.vue
@@ -5,6 +5,16 @@
       :item="item"
       :key="item.id"
     />
+
+    <div class="deposit-list__btn-more-wrp" v-if="!isLoaded">
+      <button
+        class="app__button"
+        @click="loadNext"
+        :disabled="isLoaded"
+      >
+        More
+      </button>
+    </div>
   </div>
 </template>
 
@@ -14,23 +24,49 @@ import DepositListItem from './Deposit.ListItem'
 import { issuanceService } from '@/js/services/issuances.service'
 import { IssuanceRecord } from '@/js/records/issuance.record'
 
+import config from '../../../config'
+
 export default {
   components: { DepositListItem },
   data: _ => ({
-    list: []
+    list: [],
+    next: () => {},
+    isLoaded: false,
+    isLoading: false
   }),
   created () {
     this.loadList()
   },
   methods: {
     async loadList () {
-      const { records } = await issuanceService.loadPendingIssuances()
-      this.list = records.map(r => new IssuanceRecord(r))
+      this.isLoading = true
+      const response = await issuanceService.loadPendingIssuances()
+      this.isLoading = false
+      this.updateList(response)
+    },
+    async loadNext () {
+      this.isLoading = true
+      const response = await this.next()
+      this.isLoading = false
+      this.updateList(response)
+    },
+    updateList ({ records, next }) {
+      this.list = this.list.concat(records.map(r => new IssuanceRecord(r)))
+      this.next = next
+
+      if (records.length < config.TRANSACTIONS_PER_PAGE) {
+        this.isLoaded = true
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~@scss/variables";
 
+.deposit-list__btn-more-wrp {
+  text-align: center;
+  margin-top: 1 * $point;
+}
 </style>
