85b9ed8c85db371be4bfc24d2d7b76014e1f6163
Vladislav <logvinenko.js@gmail.com>
2018-12-05 16:29:50 +0200

    Sort references by createdAt

--- src/vue/app/docReferences/reference-list-getter.js
+++ src/vue/app/docReferences/reference-list-getter.js
@@ -1,11 +1,19 @@
 import { ReferenceRecord } from '../../../js/records/reference.record'
 import { Sdk } from '../../../sdk'
+import moment from 'moment'
 
 export default {
   methods: {
     async getReferenceList () {
       const response = await Sdk.horizon.references.getAll()
-      return response.data.map(r => new ReferenceRecord(r))
+      return response.data
+        .map(r => new ReferenceRecord(r))
+        .sort((referenceA, referenceB) => {
+          return moment(referenceA.createdAt).isAfter(
+            moment(referenceB.createdAt)
+          ) ? 1
+            : -1
+        })
     }
   }
 }
